75647cd6043ecc8e58bac1ccb2de539a931ff1b7
Hady Matar <hadyematar@gmail.com>
2022-10-19 08:19:59 -0600

    complete spicy

--- code_challenges/spicy.rb
+++ code_challenges/spicy.rb
@@ -1,25 +1,26 @@
 # Write a Ruby program that prints out a String or Integer: The printed value will depend on the value of an Integer. If the Integer is a multiple of 3, print "Fizz". If the Integer is a multiple of 5, print "Buzz". If the Integer is a multiple of both 3 and 5, print "FizzBuzz". If the Integer is not a multiple of either, print the Integer itself.
 
-random_numb = rand(1000000)
-
+random_number = rand(10000000000000)
 
 if 
-    random_numb % 3 == 0 && random_numb % 5 == 0
+random_number % 3 == 0 && random_number % 5 == 0 
 
-    print "Fizzbuzz"
+print "Fizzbuss"
 
 elsif 
-    
-    random_numb % 3 == 0
+
+    random_number % 3 == 0 
 
     print "fizz"
 
 elsif 
-    random_numb % 5 == 0 
+
+    random_number % 5 == 0 
+
     print "Buzz"
 
 else 
-    print random_numb
+
+    print random_number
 
 end 
-
