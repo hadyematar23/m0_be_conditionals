c70424b96e1af550dbee110db643969061c450fb
Hady Matar <hadyematar@gmail.com>
2022-10-18 21:05:58 -0600

    Complete Part 1 and some of Part 2

--- comparisons.rb
+++ comparisons.rb
@@ -7,7 +7,7 @@
 # Make sure to run the file in your command line. Look back
 # at the directions from Section 1 if you need a refresher on how to do that.
 
-Test change
+
 #-------------------
 # PART 1: Comparing variables
 #-------------------
@@ -17,32 +17,39 @@ number_students = 20
 string_teachers = "4"
 numberDogs = 0
 
-# EXAMPLE: print the result of the comparison: is number_seachers greater than number_students?
+# EXAMPLE: print the result of the comparison: is number_teachers greater than number_students?
 puts "Is number_teachers greater than number_students?", number_teachers > number_students
 # this should print: "Is numberTeachers greater than numberStudents?" false
 
 # YOU DO: print the result of the comparison: is number_teachers less than number_students?
-# this should print: true
+puts "Is number_teachers less than number_students?", number_teachers < number_students
+# this should print: "Is number_teachers less than number_students?" true
 
 # YOU DO: print the result of the comparison: is number_teachers equal to string_teachers?
+puts "Is number_teachers equal to string_teachers?", number_teachers == string_teachers
 # this should print: false
 
 # YOU DO: print the result of the comparison: is number_teachers not equal to number_students?
+puts "Is number_teachers not equal to number_students?", number_teachers != number_students
 # this should print: true
 
 # YOU DO: print the result of the comparison: is number_students greater than or equal to 20?
+puts "Is number_students greater than or equal to 20?", number_students >= 20 
 # this should print: true
 
 # YOU DO: print the result of the comparison: is number_students greater than or equal to 21?
+puts "Is number_students greater than or equal to 21?", number_students >= 21
 # this should print: false
 
 # YOU DO: print the result of the comparison: is number_students less than or equal to 20?
+puts "Is number_students less than or equal to 20", number_students <= 20 
 # this should print: true
 
 # YOU DO: print the result of the comparison: is number_students less than or equal to 21?
+puts "Is number_students less than or equal to 21?", number_students <= 21
 # this should print: true
 
-
+puts "BREAK TO PART 2"
 #-------------------
 # PART 2: Articulating what you are doing
 #-------------------
@@ -54,10 +61,12 @@ puts "Is number_teachers greater than number_students?", number_teachers > numbe
 
 puts 4 < 9
 #YOU DO: Explain.
+puts "This line of code is asking the computer to evaluate whether the integer numbered '4' is less than the integer numbered '9'. It is then asking the computer to print that evaluation to the console. The statement will evaluate as true." 
 
 books = 3
 puts 4 < books
 # YOU DO: Explain.
+puts "This line of code is assigning the the integer '3' to the variable 'books' and then asking the computer to evaluate whether the integer '4' is less than the assignment to the variable 'books.' Finally, it is asking the computer to print that result out to the console."
 
 friends = 6
 siblings = 2
