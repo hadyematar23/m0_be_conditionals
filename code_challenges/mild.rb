5fcc496360145a5ae4ebc207894f5c41c07c907d
Hady Matar <hadyematar@gmail.com>
2022-10-19 07:06:43 -0600

    Complete mild challenge

---
+++ code_challenges/mild.rb
@@ -0,0 +1,17 @@
+# Write a Ruby program that defines a variable that stores an Integer. 
+# The program should print out the String "even" if the Integer is even,
+# and the String "odd" if the Integer is odd.
+
+random_number = rand(10000)
+
+random_number
+
+if 
+    random_number.even? 
+    print "even"
+
+else 
+    random_number.odd? 
+    print "odd"
+
+end 
